c324a8b78e26317bcfc04a29df14c8dd2936c384
Joe Fass <joseph.fass@gmail.com>
2022-12-10 15:09:04 -0800

    adding config to allow multiple quick taps of any key (since they\'re all layer tap keys) to generate multiple of the layer 0 keycode, instead of automatically changing second quick tap to a hold ... unfortunately, this disallows auto-repeat ... oh well

---
+++ keyboards/handwired/tkk/keymaps/default/config.h
@@ -0,0 +1,8 @@
+// Copyright 2022 Joe Fass (@jfass)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// Allow quick double tap and hold (don't release 2nd keypress) to trigger layer on second tap.
+// This disallows auto-repeat, but would allow quick tapping for repeated characters.
+#define TAPPING_FORCE_HOLD
+#define RETRO_TAPPING
+
